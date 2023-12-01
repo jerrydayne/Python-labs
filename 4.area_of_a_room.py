311e6e2333e607cc3c66ab555263e89086e15f80
jerrydayne <jerrydaexplorer@gmail.com>
2023-12-01 08:19:47 +0100

    area of a room

---
+++ 4.area_of_a_room.py
@@ -0,0 +1,9 @@
+## area (in sqaure feet) is equal to the product of Length and Width (in feet)
+
+length = float(input("Enter the length of the room :"))
+width = float(input("Enter the width of the room: "))
+
+area = length * width
+print("length = ",length)
+print("width = ",width)
+print("the area of the room is ",area,"Sq")
