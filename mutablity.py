c6c34ca0cc572256f45392a525b8aae7d5d01daf
Jerrydayne <jerrydaexplorer@gmail.com>
2024-02-19 00:45:18 +0100

    mutability

---
+++ mutablity.py
@@ -0,0 +1,10 @@
+filenames = ["1. Raw Data.txt", "2. Reports.txt", "3. Presentations.txt"]
+
+for filename in filenames :
+    filename = filename.replace('.', '-', 1)
+    print(filename)
+
+#strings are immutale
+#tuples are immutable
+#list are mutable
+    
