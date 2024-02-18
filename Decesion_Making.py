86752a2240f1739cb5e1faa46ace12f5d79ca2a7
Jerrydayne <jerrydaexplorer@gmail.com>
2024-02-18 21:22:02 +0100

    edditing list of items

--- Decesion_Making.py
+++ Decesion_Making.py
@@ -12,9 +12,15 @@ while True :
             for item in todos :
                 item = item.title()
                 print(item)
+        case 'edit' :
+            todo_serial_number = int(input("enter the serial number of the item to edit : "))
+            todo_serial_number = todo_serial_number - 1
+            new_todo = input("Enter new task")
+            todos[todo_serial_number] = new_todo
+            print(todos[todo_serial_number], "is replaced with ", new_todo)
         case 'exit' :
             break
-#        case something_else :
-#            print("this is an unknown selection, check again")
+        case something_else :
+            print("this is an unknown selection, check again")
 
 print("Thanks for coming!")
