cc59f2f2f69a9a933de43c32aca2352faebed40e
Jerrydayne <jerrydaexplorer@gmail.com>
2024-02-26 23:43:33 +0100

    create multiple files from a list of itms

---
+++ multiple_files.py
@@ -0,0 +1,11 @@
+file_contents = ["Local is not fake",
+                "Boys are not smiling",
+                "The good, the bad and the ugly"]
+
+file_names = ["dayne.txt",
+              "terry.txt",
+              "movie.txt"]
+
+for file_content, file_name in zip(file_contents, file_names) : 
+    file =open(f"files/{file_name}", "w")
+    file.write(file_content)
