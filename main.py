233980ccd8250b2d85e66edd58835509f8c4fd5e
Jerrydayne <jerrydaexplorer@gmail.com>
2024-03-12 21:42:03 +0100

    conditionals and slicing and boolean operators

--- main.py
+++ main.py
@@ -2,71 +2,71 @@ while True :
     user_action = input("do you want to add, display, edit, complete or exit (add/display/exit): ")
     user_action = user_action.strip()
 
-    match user_action :
-        case 'add' :
-            todo = input("Add what you want todo : ") + "\n"
-            with open('files/todos.txt', 'r') as file_reader :
-                todos = file_reader.readlines()
+    if 'add' in user_action or'new' in user_action :
+        todo = user_action[4:] + "\n"
+        with open('files/todos.txt', 'r') as file_reader :
+            todos = file_reader.readlines()
 
-            todos.append(todo)
-            with open('files/todos.txt', 'w') as file_writer :
-                file_writer.writelines(todos)
-            
-        case 'display' | 'show':
+        todos.append(todo)
+        with open('files/todos.txt', 'w') as file_writer :
+            file_writer.writelines(todos)
 
-            with open('files/todos.txt', 'r') as file_reader :
-                todos = file_reader.readlines()
+        print(f"{todo} has been added successfully to the Todo list")
+        
+    elif 'show' in user_action :
 
-            """
-            new_todos = []
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item)
-            """
-            ### new_todos = [item.strip('\n') for item in todos]
+        with open('files/todos.txt', 'r') as file_reader :
+            todos = file_reader.readlines()
 
-            for index, item in enumerate(todos) : 
-                item = item.strip('\n')
-                item = item.title()
-                item_row = f"{index + 1}.{item}"
-                print(item_row)
+        """
+        new_todos = []
+        for item in todos:
+            new_item = item.strip('\n')
+            new_todos.append(new_item)
+        """
+        ### new_todos = [item.strip('\n') for item in todos]
 
-        case 'edit' :
-            todo_serial_number = int(input("enter the serial number of the item to edit : "))
-            todo_index = todo_serial_number - 1
-            todo_to_be_edited = todos[todo_index].strip('\n')
-            todo_serial_number = todo_index
+        for index, item in enumerate(todos) : 
+            item = item.strip('\n')
+            item = item.title()
+            item_row = f"{index + 1}.{item}"
+            print(item_row)
 
-            with open('files/todos.txt', 'r') as file_reader :
-                todos = file_reader.readlines()
+    elif  'edit' in user_action :
+        todo_serial_number = int(user_action[5:])
+        todo_index = todo_serial_number - 1
+        todo_to_be_edited = todos[todo_index].strip('\n')
+        todo_serial_number = todo_index
 
-            new_todo = input("Enter new task you would want to do : ")
-            todos[todo_serial_number] = new_todo + '\n'
+        with open('files/todos.txt', 'r') as file_reader :
+            todos = file_reader.readlines()
 
-            with open('files/todos.txt', 'w') as file_writer :
-                file_writer.writelines(todos)
+        new_todo = input("Enter new task you would want to do : ")
+        todos[todo_serial_number] = new_todo + '\n'
 
-            print(todo_to_be_edited, "is replaced with ", new_todo)
+        with open('files/todos.txt', 'w') as file_writer :
+            file_writer.writelines(todos)
 
-        case 'complete' :
-             todo_serial_number = int(input("enter the serial number of the item to complete : "))
+        print(todo_to_be_edited, "is replaced with ", new_todo)
 
-             with open('files/todos.txt', 'r') as file_reader :
-                todos = file_reader.readlines()
-             todo_index = todo_serial_number -1
-             todo_to_be_removed = todos[index].strip('\n')
-             todos.pop(todo_index)
+    elif  'complete' in user_action or 'done' in user_action :
+        todo_serial_number = int(user_action[9:])
+        with open('files/todos.txt', 'r') as file_reader :
+            todos = file_reader.readlines()
+            todo_index = todo_serial_number -1
+            todo_to_be_removed = todos[todo_index].strip('\n')
+            todos.pop(todo_index)
 
-             with open('files/todos.txt', 'w') as file_writer :
-                file_writer.writelines(todos) 
+        with open('files/todos.txt', 'w') as file_writer :
+            file_writer.writelines(todos) 
 
-             message = f"Task {todo_to_be_removed} is completed and removed from the list"
-             print(message)
+            message = f"Task '{todo_to_be_removed}' is completed and removed from the list. enter 'show' to view the updated list"
+            print(message)
 
-        case 'exit' :
-            print("Bye")
-            break
-        case something_else :
-            print("this is an unknown selection, pleas check and try again")
+    elif 'exit' in user_action :
+        print("Bye")
+        break
+    else :
+        print("this is an unknown selection, pleas check and try again")
 
 print("Thanks for coming!")
