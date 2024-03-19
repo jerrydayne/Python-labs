e5dd62d87eb2042d76e60ef6c66778d98ed8e6b5
Jerrydayne <jerrydaexplorer@gmail.com>
2024-03-19 23:09:00 +0100

    adde doc-strings

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_todos(file_path='files/todos.txt') :
+    """Gets the list of to-do's from the file todo.txt"""
     with open(file_path, 'r') as file_reader :
         todos_local = file_reader.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, file_path='files/todos.txt'):
+    """writes the added to-do item to the list of to-do's file i.e doto.txt in this case"""
     with open(file_path, 'w') as file_writer:
         file_writer.writelines(todos_arg)
 
