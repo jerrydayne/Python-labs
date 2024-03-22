3d3077a64c28ae09b70a6d0658afe5c78fbf925d
Jerrydayne <jerrydaexplorer@gmail.com>
2024-03-23 00:54:48 +0100

    quiz_app completed

--- quiz_app/quiz_app.py
+++ quiz_app/quiz_app.py
@@ -4,5 +4,33 @@ with open("questions.json", 'r') as file_reader:
     content = file_reader.read()
 
 data = json.loads(content)
+score = 0
+for index, question in enumerate(data):
+    print(index + 1, question["question_text"])
+    for i, alternative in enumerate(question["alternatives"]):
+        print(i + 1, ".", alternative)
+    user_choice = int(input("Select answer by entering number of your choice : "))
+    question["user_choice"] = user_choice
 
-print(data)
+    if question["correct_answer"] == question["user_choice"]:
+        score = score + 1
+        result = "Good Job!"
+        message = f"{result} You answered question {index + 1} correctly"
+    else:
+        result = "Wrong!"
+        message = f"{result} You selected '{question['user_choice']} instead of {question['correct_answer']}"
+    print(message)
+
+"""score = 0
+
+for index, question in enumerate(data):
+    if question["correct_answer"] == question["user_choice"]:
+        score = score + 1
+        message = f"you answered question {index + 1} correctly"
+    else:
+        message = f"for question {index + 1} you selected '{question['user_choice']} instead of {question['correct_answer']}"
+    print(message)"""
+if score == len(data):
+    print("excellent! You answered all ", len(data), "correctly")
+else:
+    print("Your final score is :", score, "/", len(data))
