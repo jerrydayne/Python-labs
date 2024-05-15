5b19425ea82a87b27cb0e81ef2200081e1c0aff7
jerrydayne <jerrydaexplorer@gmail.com>
2024-05-15 18:53:16 +0100

    just added FreeSimpleGUI

--- gui.py
+++ gui.py
@@ -1,1 +1,11 @@
 from modules import functions
+
+import FreeSimpleGUI as fsg
+
+label = fsg.Text("type in a Todo here")
+input_box = fsg.InputText(tooltip="Enter a Todo")
+add_button = fsg.Button("Add")
+
+window = fsg.Window('Dayne Todo App', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
