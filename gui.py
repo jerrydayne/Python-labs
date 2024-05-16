ac1b01a8d2b4c5f805fd84893049c74135221d1e
jerrydayne <jerrydaexplorer@gmail.com>
2024-05-16 21:10:08 +0100

    Added the add_button functionality to add todos to the todos_txt file

--- gui.py
+++ gui.py
@@ -3,9 +3,27 @@ from modules import functions
 import FreeSimpleGUI as fsg
 
 label = fsg.Text("type in a Todo here")
-input_box = fsg.InputText(tooltip="Enter a Todo")
+input_box = fsg.InputText(tooltip="Enter a Todo", key='todo')
 add_button = fsg.Button("Add")
 
-window = fsg.Window('Dayne Todo App', layout=[[label], [input_box, add_button]])
-window.read()
+window = fsg.Window('Dayne Todo App',
+                    layout=[[label], [input_box, add_button]],
+                    font=('helvetica', 20))
+
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add' :
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case fsg.WIN_CLOSED :
+            break
+
+
+
 window.close()
