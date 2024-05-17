ac1c4ebb3db084ecc21ff9c032dc22c8de611b8a
jerrydayne <jerrydaexplorer@gmail.com>
2024-05-17 16:16:15 +0100

    added the edit button and implemented the function

--- gui.py
+++ gui.py
@@ -5,23 +5,40 @@ import FreeSimpleGUI as fsg
 label = fsg.Text("type in a Todo here")
 input_box = fsg.InputText(tooltip="Enter a Todo", key='todo')
 add_button = fsg.Button("Add")
+list_box = fsg.Listbox(values=functions.get_todos(), key='todos',
+                       enable_events=True, size=[45, 10])
+edit_button = fsg.Button("Edit")
+
 
 window = fsg.Window('Dayne Todo App',
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font=('helvetica', 20))
 
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, 'evet = ', event)
+    print(2, 'values = ', values)
+    print(3, 'selected todo', values['todos'])
     match event:
-        case 'Add' :
+        case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
-        case fsg.WIN_CLOSED :
+            window['todos'].update(values=todos)
+
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case fsg.WIN_CLOSED:
             break
 
 
