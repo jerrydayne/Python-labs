d540fb84bdef3a3ac6ea6618ca5c53f7cc302e9b
jerrydayne <jerrydaexplorer@gmail.com>
2024-06-05 22:38:14 +0100

    added exit btn and exit functionality

--- archive_extractor/app_interface.py
+++ archive_extractor/app_interface.py
@@ -9,6 +9,7 @@ select_btn = fsg.FileBrowse("Choose Archive", key="archive")
 
 dir_label = fsg.Text("Select where to save extracted archive")
 dir_input = fsg.Input()
+exit_btn = fsg.Button("Exit")
 dir_btn = fsg.FolderBrowse("Choose Location", key="destination")
 
 extract_tbn = fsg.Button("Extract")
@@ -17,11 +18,18 @@ status_label = fsg.Text(key="status", text_color="white")
 window = fsg.Window("Dayne Archive Extractor",
                     layout=[[select_label, select_input, select_btn],
                             [dir_label, dir_input, dir_btn],
-                            [extract_tbn, status_label]])
+                            [extract_tbn, exit_btn],
+                            [status_label]])
 
 while True:
     event, values = window.read()
     print(event, values)
+
+    match event:
+        case "Exit":
+            break
+        case fsg.WIN_CLOSED:
+            break
 
     archive_path = values["archive"]
     dest_folder = values["destination"]
